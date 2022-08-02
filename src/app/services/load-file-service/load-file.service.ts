3c032654cc808bf79deefbdb2e295dce0ecc7508
Fábry Dávid <fabry.david@developit.hu>
2022-08-02 15:38:01 +0200

    map load base

---
+++ src/app/services/load-file-service/load-file.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError, map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadFileService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getFile(path: string): any {
+    return this.http.get<any>(path).pipe(
+      map((response) => {
+        return response;
+      }),
+      catchError(this.handleError.bind(this))
+    )
+  }
+
+  public handleError(
+    err: HttpErrorResponse,
+    caught: Observable<any>
+  ): Observable<any> {
+    return throwError(err);
+  }
+}
