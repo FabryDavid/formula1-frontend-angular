8d22de3382d2fd71d7ca9e0c5fa7a17af5cc9dfb
Fábry Dávid <fabry.david@developit.hu>
2022-08-02 15:20:27 +0200

    leaflet base

--- src/app/components/pages/schedule/weekend/circuit-infos/circuit-infos.module.ts
+++ src/app/components/pages/schedule/weekend/circuit-infos/circuit-infos.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CircuitInfosComponent} from "./circuit-infos.component";
 import {SectorTimeModule} from "./sector-time/sector-time.module";
+import {GeoMapModule} from "./geo-map/geo-map.module";
 
 
 @NgModule({
   declarations: [CircuitInfosComponent],
   imports: [
     CommonModule,
-    SectorTimeModule
+    SectorTimeModule,
+    GeoMapModule
   ],
   exports: [CircuitInfosComponent]
 })
---
+++ src/app/components/pages/schedule/weekend/circuit-infos/geo-map/geo-map.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GeoMapComponent} from "./geo-map.component";
+
+
+@NgModule({
+  declarations: [GeoMapComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [GeoMapComponent]
+})
+export class GeoMapModule {
+}
