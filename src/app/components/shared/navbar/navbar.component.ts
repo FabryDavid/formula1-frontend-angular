53594dbfdbb5d42515c195c2df6ed7de7881365f
Fábry Dávid <fabry.david@developit.hu>
2022-08-09 12:41:30 +0200

    draver display fix

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { INavigationLink } from '../../../interfaces/inavigation-link';
-import { NavbarServiceService } from '../../../services/navbar-service/navbar-service.service';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {INavigationLink} from '../../../interfaces/inavigation-link';
+import {NavbarServiceService} from '../../../services/navbar-service/navbar-service.service';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -10,20 +11,37 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 })
 export class NavbarComponent implements OnInit {
   isElevated = false;
-  drawer = false;
+  drawerValue = false;
   navLinks: Array<INavigationLink> = [
-    { text: 'Schedule', url: 'schedule' },
-    { text: 'News', url: 'news' },
-    { text: 'Drivers', url: 'drivers' },
-    { text: 'Constructors', url: 'constructors' },
-    { text: 'Standings', url: 'standings' },
+    {text: 'Schedule', url: 'schedule'},
+    {text: 'News', url: 'news'},
+    {text: 'Drivers', url: 'drivers'},
+    {text: 'Constructors', url: 'constructors'},
+    {text: 'Standings', url: 'standings'},
   ];
   faArrowLeft = faArrowLeft;
   NavbarService = NavbarServiceService;
 
-  constructor(public navbarService: NavbarServiceService) {}
+  get drawer() {
+    return this.drawerValue
+  }
 
-  ngOnInit(): void {}
+  set drawer(value) {
+    document.body.setAttribute('style', value ? `overflow: hidden;` : '')
+
+    this.drawerValue = value
+  }
+
+  constructor(public navbarService: NavbarServiceService, private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.drawer = false
+      }
+    })
+  }
+
+  ngOnInit(): void {
+  }
 
   @HostListener('window:scroll', [])
   onScroll() {
